346e51ef9db0ee205fb5f463fc64446d3daa00ef
DannyRalph <danielraphael846@gmail.com>
2022-06-30 11:09:07 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry 
-  * 
-  * Return: Always 0 which is success 
-  */ 
- int main(void) 
- { 
-         puts("\"Programming is like building a multilingual puzzle"); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
