079797e8b09dcc37a11997c5d79de9202db5683f
DannyRalph <danielraphael846@gmail.com>
2022-07-01 16:05:00 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -entry point
+ * Return: Always 0 (Succrss/correct
+ */
+int main(void)
+{
+
+	int n;
+
+	while (n < 10)
+	{
+
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+return (0);
+}
