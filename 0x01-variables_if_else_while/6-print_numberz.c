59d8d2c98eb10094ecabe60dac291e40ebf6c4ff
DannyRalph <danielraphael846@gmail.com>
2022-07-01 16:42:56 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -entry point
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+
+	int n;
+
+	while (n < 10)
+	{
+
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
