eaafa42187d02a382c5f9e34f21f2582e10601dc
DannyRalph <danielraphael846@gmail.com>
2022-07-03 23:46:03 -0700

    Task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,23 +13,21 @@ int main(void)
 		for (num2 = 0; num2 < 100; num2++)
 		{
 
-			if (num1 < num2)
+			putchar(num1 / 10 + 48);
+			putchar(num1 % 10 + 48);
+			putchar(' ');
+			putchar(num2 / 10 + 48);
+			putchar(num2 % 10 + 48);
+			if (num1 == 98 && num2 == 99)
 			{
 
-				putchar(num1 / 10 + 48);
-				putchar(num1 % 10 + 48);
-					putchar(' ');
-				putchar(num2 / 10 + 48);
-				putchar(num2 % 10 + 48);
-				if (num1 != 98 || num2 != 99)
-
-					putchar(',');
-				putchar(' ');
+				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
 
-return (0);
-
+	return (0);
 }
