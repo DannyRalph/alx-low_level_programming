38d67f93b2c28bee4646eb94107ef15e7579d4cd
DannyRalph <danielraphael846@gmail.com>
2022-07-03 21:29:28 -0700

    Taskdone

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,11 +16,11 @@ int main(void)
 			if (num1 < num2)
 			{
 
-				putchar(num1 / 10 + '0');
-				putchar(num1 % 10 + '0');
+				putchar(num1 / 10 + 48);
+				putchar(num1 % 10 + 48);
 					putchar(' ');
-				putchar(num2 / 10 + '0');
-				putchar(num2 % 10 + '0');
+				putchar(num2 / 10 + 48);
+				putchar(num2 % 10 + 48);
 				if (num1 != 98 || num2 != 99)
 
 					putchar(',');
