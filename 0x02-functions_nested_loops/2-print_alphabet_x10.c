d1cb017cff662d5161d1000d6cbe65891afc47c5
DannyRalph <danielraphael846@gmail.com>
2022-07-05 07:25:07 -0700

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in a row
+ *
+ * Return Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int num;
+
+
+	for (num = 0; num < 10; num++)
+	{
+		for (alphabet = 'a'; alphabet <= z; alphabet)
+		{
+			_putchar(alphabet);
+		}
+
+		_putchar('\n');
+	}
+
+
+}
