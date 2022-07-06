e682c80f6341296251d019753d5427b99890c431
DannyRalph <danielraphael846@gmail.com>
2022-07-05 21:02:47 -0700

    Task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,16 +13,16 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		last_digit = (-1  * (n % 10));
-		_putchar(last_digit + '0');
-		return (last_digit);
+		last_num = (-1  * (n % 10));
+		_putchar(last_num + '0');
+		return (last_num);
 	}
 
 	else
 	{
-		last_digit = (n % 10);
-		_putchar(last_digit + '0');
-		return (last_digit);
+		last_num = (n % 10);
+		_putchar(last_num + '0');
+		return (last_num);
 	}
 
 }
