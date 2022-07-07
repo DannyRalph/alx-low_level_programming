c08c897075b9ecfb3b9d0d429eedcbaa7bbfae2c
DannyRalph <danielraphael847@gmail.com>
2022-07-07 13:14:22 -0700

    Task8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,16 +6,19 @@
  */
 void print_square(int size)
 {
-	int m;
+	int m, n;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (m = 0; m < size; m++)
+		for (m = 0; m <= size; m++)
+		{
+		for (n = 0; n < size; n++)
 		{
 			_putchar(35);
 		}
 		_putchar('\n');
+	}
 	}
 }
