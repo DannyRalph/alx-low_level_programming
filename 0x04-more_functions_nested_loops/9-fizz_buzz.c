803779e324a809ca865c3c3021b6af53e9aab079
DannyRalph <danielraphael847@gmail.com>
2022-07-07 15:25:06 -0700

    Task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int m = 100;
+	int n;
+
+	for (n = 1; n <= m; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+		{
+			if (n < m)
+				printf("Buzz ");
+			else
+				print("Buzz");
+		}
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
+}
