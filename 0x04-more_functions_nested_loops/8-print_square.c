17dbde6ef2b399698cbe4bb7989b06065f3209b0
DannyRalph <danielraphael847@gmail.com>
2022-07-07 12:30:32 -0700

    Task5

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: integer parameter
+ */
+void print_square(int size)
+{
+	int m;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (m = 0; m < size; m++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
