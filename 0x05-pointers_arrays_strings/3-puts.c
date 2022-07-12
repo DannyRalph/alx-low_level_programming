5c1abd944980b8146df5e13e41f1dc9e962018a4
DannyRalph <danielraphael847@gmail.com>
2022-07-12 08:25:34 -0700

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new lin
+ * @str: integer parameter
+ * Return: Always success
+ */
+
+void _puts(char *str)
+{
+	char str1[];
+
+	puts(str1);
+
+	_putchar('\n');
+}
