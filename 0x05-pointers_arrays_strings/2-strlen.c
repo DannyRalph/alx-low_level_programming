00535c4bc4ec9fd4922fdcfc6e583d240e82a5c4
DannyRalph <danielraphael847@gmail.com>
2022-07-12 08:05:25 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: integer parameter
+ * Return: Always Success
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+
+	return (n);
+}
