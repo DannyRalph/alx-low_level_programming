14aba5bb015928ef3e9081eed8c7f9962841d5ef
DannyRalph <danielraphael847@gmail.com>
2022-07-18 07:36:11 -0700

    Project 3 continued

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -23,7 +23,7 @@ unsigned int _strspn(char *s, char *accept)
 				bytes++;
 				break;
 			}
-			else if (accept[index] == '\0')
+			else if (accept[index + 1] == '\0')
 				return (bytes);
 		}
 		s++;
